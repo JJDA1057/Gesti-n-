3c83f19a952d0689ddcac71bb5cedb00915bb3ec
Juan Jose <juanjduque10@gmail.com>
2024-05-04 00:22:14 -0500

    Interface,componentes,repositorio

---
+++ GestionDeProyectos/GestionWEB/Repositorios/IRepositorio.cs
@@ -0,0 +1,20 @@
+﻿namespace GestionWEB.Repositorios
+{
+    public interface IRepositorio
+    {
+        Task<HttpResponseWrapper<T>> GetAsync<T>(string url);
+
+        Task<HttpResponseWrapper<object>> PostAsync<T>(string url, T model);
+
+        Task<HttpResponseWrapper<TActionResponse>> PostAsync<T, TActionResponse>(string url, T model);
+
+        Task<HttpResponseWrapper<object>> DeleteAsync<T>(string url);
+
+        Task<HttpResponseWrapper<object>> PutAsync<T>(string url, T model);
+
+        Task<HttpResponseWrapper<TActionResponse>> PutAsync<T, TActionResponse>(string url, T model);
+
+
+    }
+
+}
